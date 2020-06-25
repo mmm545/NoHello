c746a30a3c79a3f605946d2a701f2f4ed710b75f
mmm545 <64874108+mmm545@users.noreply.github.com>
2020-06-25 14:39:36 +0300

    Update Main.php

--- src/mmm545/NoHello/Main.php
+++ src/mmm545/NoHello/Main.php
@@ -14,11 +14,25 @@ class Main extends PluginBase implements Listener{
            $this->getServer()->getPluginManager()->registerEvents($this, $this);
        }
 
+    /*public function onChatEvent(PlayerChatEvent $event){
+        $event->setCancelled(true);
+        $format = $event->getFormat();
+        $msg = $event->getMessage();
+        $this->getServer()->broadcastMessage('<'. $event->getPlayer()->getName().'>'. $msg);
+        $words = ["hello", "hi"];
+        $msg = $event->getMessage();
+        foreach($words as $word){
+            if(preg_match('/\b'.$word.'\b/i', $msg)){
+                $this->getServer()->broadcastMessage(TF::AQUA . "[NoHello] Don't just say hello/hi in the chat " . $event->getPlayer()->getName());
+            }
+        }
+    }*/
+
        public function onChatEvent(PlayerChatEvent $event){
            $words = ["hello", "hi"];
            $msg = $event->getMessage();
            foreach($words as $word){
-               if(stripos($msg, $word) !== false){
+               if(preg_match('/\b'.$word.'\b/i', $msg)){
                    $this->getServer()->broadcastMessage(TF::AQUA . "[NoHello] Don't just say hello/hi in the chat " . $event->getPlayer()->getName());
                 }
            }
